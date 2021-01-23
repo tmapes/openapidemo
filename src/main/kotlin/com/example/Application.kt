17c122b80296cc8da4450b1cb8ac0e8e9ff3c546
Taylor M <taylor@mapes.info>
2021-01-23 17:08:49 +0000

    Initial commit

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,21 @@
+package com.example
+
+import io.micronaut.runtime.Micronaut.*
+import io.swagger.v3.oas.annotations.*
+import io.swagger.v3.oas.annotations.info.*
+
+@OpenAPIDefinition(
+    info = Info(
+            title = "openapidemo",
+            version = "0.0"
+    )
+)
+object Api {
+}
+fun main(args: Array<String>) {
+	build()
+	    .args(*args)
+		.packages("com.example")
+		.start()
+}
+
